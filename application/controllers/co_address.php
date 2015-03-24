39fc5c5add6fb949c4210371f8b657235a1baa50
Alizzabeth <Alizzabeth@sutthidas-MacBook-Pro.local>
2015-03-24 10:03:32 +0700

    change default route on route.php and create co_address controller to load view search.php for testing codeigniter.

---
+++ application/controllers/co_address.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+* 
+*/
+class co_address extends CI_Controller
+{
+	
+	public function index()
+	{
+		$this->load->view('search');
+	}
+}
+
+ ?>
---
+++ application/views/search.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Search</title>
+</head>
+<body>
+	<h1>Success</h1>
+</body>
+</html>
